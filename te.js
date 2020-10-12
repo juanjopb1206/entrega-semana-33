da5c2323c042c58409bd6cbee7ae1889552c03fd
juanjopb1206 <70530399+juanjopb1206@users.noreply.github.com>
2020-10-11 20:19:49 -0500

    Add files via upload

---
+++ te.js
@@ -0,0 +1,80 @@
+var cortinasa=function(){
+
+		var b=document.getElementById("bt")
+		var b1=document.getElementById("bta")
+		var cr1=document.getElementById("c1")	
+		var cr2=document.getElementById("c2");
+		var n2=document.getElementById("hi");
+		var m2=document.getElementById("gi");
+		var d1=document.getElementById("dbta")
+		
+		cr1.className="cor001";
+		cr2.className="cor002";
+		
+		b1.className="nada";
+		d1.className="nada";
+		b.className="botonfin"
+
+		n2.className="hongo";
+		m2.className="gota";
+
+	}
+
+var mth=function(){
+
+		
+		var n2=document.getElementById("hi");
+	
+		
+		n2.className="hongo1";
+		
+
+	}
+
+var mtg=function(){
+
+		var m2=document.getElementById("gi");
+	
+		
+		m2.className="gota1";
+	
+
+	}
+
+var t=function(){
+
+		var t1=document.getElementById("th1")	
+		var t2=document.getElementById("th");
+	
+		
+		t2.className="techo1";
+	
+
+	}
+
+var cortinas=function(){
+
+		var b=document.getElementById("bt")
+		var cr1=document.getElementById("c1")	
+		var cr2=document.getElementById("c2");
+	
+		
+		cr1.className="cor01";
+		cr2.className="cor02";
+
+		b.className="nada";
+
+	}
+
+
+var h=function(){
+
+		var h1=document.getElementById("ha1")	
+		var h2=document.getElementById("ha");
+	
+		
+		ha.className="hada1";
+	
+
+	}
+
